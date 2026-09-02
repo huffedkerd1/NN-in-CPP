2aa79693a5ecedae8b95c243b044c49cc6d62c48
huffedkerd1 <huffedkerd1@gmail.com>
2026-09-03 00:32:30 +0500

    Working on Memory Layout

---
+++ tensor/base/storage.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+
+using Scalar = float;
+using Size = size_t;
+
+using FloatVec = std::vector<Scalar>;
+using IntVec = std::vector<int>;
+
+class Storage
+{
+private:
+    Scalar *data_ = nullptr;
+    Size numel_ = 0;
+
+public:
+    Storage(const FloatVec &input_data);
+    ~Storage();
+
+    Storage(const Storage &other);
+    Storage &operator=(const Storage &other);
+
+    Storage(Storage &&other);
+    Storage &operator=(Storage &&other);
+};
---
+++ tensor/src/storage.cpp
@@ -0,0 +1,80 @@
+#include "../base/storage.hpp"
+
+#include <vector>
+
+Storage::Storage(
+    const FloatVec &input_data)
+    : data_(nullptr),
+      numel_(0)
+{
+    numel_ = input_data.size();
+
+    data_ = new Scalar[numel_];
+    for (Size i = 0; i < numel_; ++i)
+    {
+        data_[i] = input_data[i];
+    }
+}
+
+Storage::~Storage()
+{
+    delete[] data_;
+}
+
+Storage::Storage(const Storage &other)
+    : numel_(other.numel_)
+{
+    data_ = new Scalar[numel_];
+
+    for (Size i = 0; i < numel_; ++i)
+    {
+        data_[i] = other.data_[i];
+    }
+}
+
+Storage &Storage::operator=(const Storage &other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    delete[] data_;
+
+    this->numel_ = other.numel_;
+
+    data_ = new Scalar[numel_];
+
+    for (Size i = 0; i < numel_; ++i)
+    {
+        data_[i] = other.data_[i];
+    }
+
+    return *this;
+}
+
+Storage::Storage(Storage &&other)
+    : numel_(other.numel_),
+      data_(other.data_)
+{
+    other.data_ = nullptr;
+    other.numel_ = 0;
+}
+
+Storage &Storage::operator=(Storage &&other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    delete[] data_;
+
+    this->numel_ = other.numel_;
+    this->data_ = other.data_;
+
+    other.data_ = nullptr;
+    other.numel_ = 0;
+
+    return *this;
+}
